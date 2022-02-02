ef6ece6f3e255f4112eb53f7095aef06bd703b22
oneyottabyte <junhinhodorian@gmail.com>
2022-02-02 00:15:30 -0300

    first commit

---
+++ src/main/java/br/com/dorian/fluke/FlukeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dorian.fluke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlukeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlukeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dorian/fluke/FlukeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dorian.fluke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlukeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
