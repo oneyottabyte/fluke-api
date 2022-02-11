7631f293804b3ddfc0997816d72317d12bdd8542
oneyottabyte <junhinhodorian@gmail.com>
2022-02-11 02:43:51 -0300

    Pesquisa por cpf

--- src/main/java/br/com/dorian/fluke/repository/cliente/ClienteRepository.java
+++ src/main/java/br/com/dorian/fluke/repository/cliente/ClienteRepository.java
@@ -1,10 +1,14 @@
 package br.com.dorian.fluke.repository.cliente;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.dorian.fluke.model.apolice.Apolice;
 import br.com.dorian.fluke.model.cliente.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 	boolean existsByCpf(String cpf);
+	Optional<Apolice> findByCpf(String cpf);
 }
