591a3cb9e0af22129b2d831bc50da333940f207c
oneyottabyte <junhinhodorian@gmail.com>
2022-02-12 16:15:05 -0300

    adicionando tags swagger

--- src/main/java/br/com/dorian/fluke/controller/v1/cliente/ClienteController.java
+++ src/main/java/br/com/dorian/fluke/controller/v1/cliente/ClienteController.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,16 +24,22 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.dorian.fluke.controller.v1.dto.ClienteDTO;
 import br.com.dorian.fluke.model.cliente.Cliente;
 import br.com.dorian.fluke.service.cliente.ClienteService;
-import br.com.dorian.fluke.util.config.CpfValidator;
+import br.com.dorian.fluke.util.cpf.CpfValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/clientes")
-@CrossOrigin(origins = "*", maxAge = 3600)
+@Tag(name = "Clientes", description = "CRUD de Clientes")
+@Schema(hidden = true)
 public class ClienteController {
 
 	@Autowired
 	private ClienteService clienteService;
 	
+	@Operation(summary = "Cadastro de cliente. Todos os dados são obrigatórios.")
 	@PostMapping
     public ResponseEntity<Object> saveCliente(@RequestBody @Valid ClienteDTO dto){
         if(clienteService.existsByCpf(dto.getCpf())){
@@ -48,23 +53,27 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.save(cliente));
     }
 
+	@Operation(summary = "Listagem de todos os clientes.")
     @GetMapping
-    public ResponseEntity<Page<Cliente>> getAllClientes(@PageableDefault(page = 0, size = 3, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+    public ResponseEntity<Page<Cliente>> getAllClientes(@PageableDefault(page = 0, size = 3, sort = "id", direction = Sort.Direction.ASC) @Parameter(hidden = true) Pageable pageable){
         return ResponseEntity.status(HttpStatus.OK).body(clienteService.findAll(pageable));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getOneCliente(@PathVariable(value = "id") Long id){
-        Optional<Cliente> clienteOptional = clienteService.findById(id);
+	@Operation(summary = "Consulta de cliente pelo CPF.")
+	@Schema(hidden = true)
+    @GetMapping("/{cpf}")
+    public ResponseEntity<Object> getOneCliente(@PathVariable(value = "cpf") String cpf){
+        Optional<Cliente> clienteOptional = clienteService.findByCpf(cpf);
         if (!clienteOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(clienteOptional);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteCliente(@PathVariable Long id){
-    	Optional<Cliente> clienteOptional = clienteService.findById(id);
+	@Operation(summary = "Remove um cliente pelo CPF.")
+    @DeleteMapping("/{cpf}")
+    public ResponseEntity<Object> deleteCliente(@PathVariable String cpf){
+    	Optional<Cliente> clienteOptional = clienteService.findByCpf(cpf);
         if (!clienteOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado.");
         }
@@ -72,12 +81,17 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.OK).body("Cliente deletado com successo.");
     }
 
+	@Operation(summary = "Atualiza um cliente pelo ID.")
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateCliente(@PathVariable Long id, @RequestBody @Valid ClienteDTO dto){
         Optional<Cliente> clienteOptional = clienteService.findById(id);
         if (!clienteOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado.");
         }
+        if(!CpfValidator.isValid(dto.getCpf())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: CPF Invalido!");
+        }
+
         var cliente = new Cliente();
         BeanUtils.copyProperties(dto, cliente);
         cliente.setId(clienteOptional.get().getId());
